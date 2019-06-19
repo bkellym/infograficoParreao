62fd9f8d58e959806f29f8031f16b1cf719e48fe
bkellym <bkellym.sm@gmail.com>
2019-06-19 04:28:57 -0300

    Aplicação do mapa arredores

--- codigo/telas/TelaInicial.js
+++ codigo/telas/TelaInicial.js
@@ -7,17 +7,17 @@ class TelaInicial{
 		this.exibicao = new Objetos(fundoExibicao, width/1.39, height/3.08, width/2.5, (width/2.5) * 16/21);
 		this.sombraExibicao = new Objetos(imgSombraExibicao, width/1.395, height/2.95, width/2.28, (width/2.28) * 16/21);
 		this.mascote = new Objetos(posMascote, width/2.8, height/2.6, width/5.3, width/5.3);
-		this.mapa = new Objetos(mapa, width/2.3, height/1.45, width * 3/4, (width * 3/4) * 3/8);
+		this.mapa = new Objetos(mapa, width/2, height/2, width, height);
 	}
 
 	exibir(){
 		background('#F1E2C3');
 
 		// Demais objetos
+		this.mapa.exibir();
 		this.mascote.exibir();
 		this.sombraExibicao.exibir();
 		this.exibicao.exibir();
-		this.mapa.exibir();
 
 		this.menu.exibir();
 
